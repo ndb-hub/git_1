dfc03df388439c23ae9e677e4ad94134931cead4
nargesdehghanbanadaki <n.dehghan123b@gmail.com>
2021-03-16 22:41:00 +0330

    your namae.

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main{
     public static void main(String [] args){
         System.out.println("Hello git");
-        System.out.println("My name is: <your name>");
+        System.out.println("My name is: <narges dehghan banadaki>");
         System.out.println("My student ID is <your id>");
     }
 }  
